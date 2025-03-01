858f7f4bd53adaed3066e3da4ae115a19c65e75a
ahmadfadlitambunan <ahmadfadlitambunan12@gmail.com>
2025-03-01 20:46:43 +0700

    MOD: Change databse table scheme

--- src/database.py
+++ src/database.py
@@ -5,7 +5,7 @@ import time
 from typing import Dict
 from sqlalchemy import text
 
-conn = st.connection('mysql', type='sql')
+conn = st.connection('chat_opa', type='sql')
 
 def save_chat_history(chat_history : Dict, conversation_data : Dict) -> Dict:
     try:
@@ -16,12 +16,12 @@ def save_chat_history(chat_history : Dict, conversation_data : Dict) -> Dict:
                 "chat_id" : chat_history["chat_id"],
                 "message_user" : chat_history["chat_messages"]["user"],
                 "message_assistant" : chat_history["chat_messages"]["assistant"],
-                "chat_histories_id" : conversation_data["id"],
-                "conversation_id" : conversation_data["conversation_id"],
+                "conversation_id" : conversation_data["id"],
+                "conversation_session_id" : conversation_data["session_id"],
                 "previous_chat_id" : chat_history["previous_chat_id"]
             }
 
-            stmt = text("INSERT INTO feedback_chat.feedback(chat_id, chat_histories_id, conversation_id, message_user, message_assistant, previous_chat_id) VALUES (:chat_id, :chat_histories_id, :conversation_id, :message_user, :message_assistant, :previous_chat_id)")
+            stmt = text("INSERT INTO chat_opa.chat_histories(chat_id, conversation_id, conversation_session_id, message_user, message_assistant, previous_chat_id) VALUES (:chat_id, :conversation_id, :conversation_session_id, :message_user, :message_assistant, :previous_chat_id)")
 
             result = session.execute(stmt, params=data)
 
@@ -29,7 +29,7 @@ def save_chat_history(chat_history : Dict, conversation_data : Dict) -> Dict:
 
             inserted_id = result.lastrowid
 
-            stmt2 = text("SELECT * FROM feedback_chat.feedback WHERE id = :id")
+            stmt2 = text("SELECT * FROM chat_opa.chat_histories WHERE id = :id")
             
             results_row = session.execute(stmt2, {"id" : inserted_id}).mappings().fetchone()
 
@@ -44,7 +44,7 @@ def get_user_by_id (user_id : int) -> Dict:
     try:
         with conn.session as session:
 
-            stmt = text("SELECT id, ip_client_id, session_id FROM feedback_chat.users WHERE users.id = :user_id")
+            stmt = text("SELECT id, ip_client_id, session_id FROM chat_opa.users WHERE users.id = :user_id")
             result = session.execute(stmt, params={"user_id": user_id}).mappings().fetchone()
         
         if result is not None:
@@ -79,12 +79,11 @@ def check_ip_already_exists(ip_address: str) -> Dict:
 def save_ip_client(ip_address : str) -> int:
     try:
         data = {
-            "ip_address" : ip_address,
-            "first_seen" : datetime.datetime.now(tz=datetime.timezone.utc)
+            "ip_address" : ip_address
         }
         
         with conn.session as session:
-            stml = text('INSERT INTO feedback_chat.ip_clients(ip_address, first_seen) VALUES (:ip_address, :first_seen)')
+            stml = text('INSERT INTO chat_opa.ip_clients(ip_address) VALUES (:ip_address)')
             result = session.execute(stml, data)
             session.commit()
 
@@ -98,11 +97,10 @@ def save_ip_login_history(ip_client_id: str) -> int:
     try:
         data = {
             "ip_client_id" : ip_client_id,
-            "log_time" : datetime.datetime.now(tz=datetime.timezone.utc)
         }
 
         with conn.session as session:
-            stml = text('INSERT INTO feedback_chat.ip_login_histories(ip_client_id, log_time) VALUES (:ip_client_id, :log_time)')
+            stml = text('INSERT INTO chat_opa.ip_login_histories(ip_client_id) VALUES (:ip_client_id)')
             result = session.execute(stml, data)
             session.commit()
 
@@ -117,11 +115,10 @@ def save_user(ip_client_id : str, session_id : str) -> int:
         data = {
             "ip_client_id" : ip_client_id,
             "session_id" : session_id,
-            "created_at" : datetime.datetime.now(tz=datetime.timezone.utc)
         }
         
         with conn.session as session:
-            stml = text('INSERT INTO feedback_chat.users(ip_client_id, session_id, created_at) VALUES (:ip_client_id, :session_id, :created_at)')
+            stml = text('INSERT INTO chat_opa.users(ip_client_id, session_id) VALUES (:ip_client_id, :session_id)')
             result = session.execute(stml, data)
             session.commit()
 
@@ -150,17 +147,17 @@ def save_new_user(ip_client : str, session_id : str) -> int:
 def save_conversation(data : Dict) -> int:
     try:
         data = {
-            "conversation_id" : data["conversation_id"],
+            "session_id" : data["conversation_id"],
             "user_id" : data["user_id"]
         }
         
         with conn.session as session:
-            stml = text('INSERT INTO feedback_chat.chat_histories(conversation_id, user_id) VALUES (:conversation_id, :user_id)')
+            stml = text('INSERT INTO chat_opa.conversations(session_id, user_id) VALUES (:session_id, :user_id)')
             result = session.execute(stml, data)
             session.commit()
 
             inserted_id = result.lastrowid
-            stmt2 = text('SELECT id, conversation_id, user_id FROM feedback_chat.chat_histories WHERE id = :id')
+            stmt2 = text('SELECT id, session_id, user_id FROM chat_opa.conversations WHERE id = :id')
             result_row = session.execute(stmt2, {"id": inserted_id}).mappings().fetchone()
             
         if result_row is not None:
@@ -173,18 +170,18 @@ def save_conversation(data : Dict) -> int:
         return 0
 
 
-def dialog_feedback_chat_on_change(index):       
+def dialog_chat_opa_on_change(index):       
     st.session_state._temp_feedback = st.session_state[f"text_area_{index}"]
 
 @st.dialog("Feedback")
-def give_feedback_chat_dialog(chat_id, index):
+def give_chat_opa_dialog(chat_id, index):
     st.write(f"Anda memberikan 👎 dari respon chatbot")
 
     feedback = st.text_area(
             label="Beritahu kami",
             placeholder="Tuliskan alasan kenapa memberikan feedback 👎",
             key=f"text_area_{index}",
-            on_change=dialog_feedback_chat_on_change,
+            on_change=dialog_chat_opa_on_change,
             args = [index]
         )
     
@@ -192,7 +189,7 @@ def give_feedback_chat_dialog(chat_id, index):
         if st.session_state._temp_feedback is not None and len(st.session_state._temp_feedback.strip()) > 0:
             # st.write(chat_id)
             # # st.write(st.session_state._temp_feedback)
-            save_text_feedback_chat(chat_id, feedback)
+            save_text_chat_opa(chat_id, feedback)
             st.success("Terimakasih atas feedback anda", icon="👍")
             st.session_state._temp_feedback = None
             time.sleep(1)
@@ -203,20 +200,20 @@ def give_feedback_chat_dialog(chat_id, index):
 def save_thumb_chat_feedback(index, id):
     with conn.session as session :
 
-        stmt = text("UPDATE feedback_chat.feedback SET thumb_score = :thumb WHERE id = :id")
+        stmt = text("UPDATE chat_opa.chat_histories SET thumb_score = :thumb WHERE id = :id")
         result = session.execute(stmt, params= {"thumb" : st.session_state[f"fb_{index}"], "id" : id})
         st.session_state.chat_histories[index]["thumb_score"] = st.session_state[f"fb_{index}"] # thumb score saved in session state
         session.commit()
         
     if st.session_state[f"fb_{index}"] == 0 :
-        give_feedback_chat_dialog(id, index)
+        give_chat_opa_dialog(id, index)
 
 
-def save_text_feedback_chat(id, feedback) :
+def save_text_chat_opa(id, feedback) :
     with conn.session as session:
-        stmt = text("INSERT INTO feedback_chat.feedback_tags(feedback_id, tag_value) VALUES (:id, :feedback)")
+        stmt = text("UPDATE chat_opa.chat_histories SET feedback = :feedback WHERE id = :id")
         result = session.execute(stmt, params={"id" : id, "feedback" : html.escape(feedback)})
-        session.commit
+        session.commit()
 
         if result.lastrowid > 0 :
             return True
